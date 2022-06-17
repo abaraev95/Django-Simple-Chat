510b0e0020810b0cdf58c9ca4c64c36b7bf9c47d
Alexander Baraev <alexander.baraev95@googlemail.com>
2022-06-17 15:10:09 +0200

    chat object added

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Chat, Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text', 'created_at', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'author', 'receiver')
+    search_fields = ('text',)
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        print('Received data: ' + request.POST['textmessage'])
+    return render(request, 'chat/index.html', {'userName': 'Alex'})
